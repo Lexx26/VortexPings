b7f373e78ba8cad43e4eb4ef94ae53ccf6ae4d9c
Lexx <losangel@yandex.ru>
2023-07-25 22:16:51 +0400

    Added MainModule

--- UIWPF/MainModule.cs
+++ UIWPF/MainModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace UIWPF
 {
-    class MainModule
+    public class MainModule:IModule
     {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
