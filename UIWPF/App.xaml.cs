dc3e9b10024a147079bf5bd1f5756dcf7c75bd3a
Lexx <losangel@yandex.ru>
2023-07-25 22:16:08 +0400

    Setup WPF project

--- UIWPF/App.xaml.cs
+++ UIWPF/App.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using UIWPF.Views;
 
 namespace UIWPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule(typeof(MainModule));
+        }
+
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindow>();
+            return window;
+        }
     }
 }
