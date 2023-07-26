9040f2c122da66339bd89b71778c2f5d842b8889
Lexx <losangel@yandex.ru>
2023-07-27 00:18:58 +0400

    Added PingGroupButton control.

---
+++ UIWPF/Controls/Custom/PingGroupButton.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VortexPings.Models;
+
+namespace UIWPF.Controls.Custom
+{
+    /// <summary>
+    /// Interaction logic for PingGroupButton.xaml
+    /// </summary>
+    public partial class PingGroupButton : UserControl
+    {
+        public static readonly DependencyProperty GroupItemProperty =
+          DependencyProperty.Register("GroupItem", typeof(NodeGroup), typeof(PingGroupPanel), new PropertyMetadata(null));
+
+        public NodeGroup NodeGroupItem
+        {
+            get { return (NodeGroup)GetValue(GroupItemProperty); }
+            set { SetValue(GroupItemProperty, value); SetGroupName(); }
+        }
+
+        public PingGroupPanel ParentPingGroupPanel { get; set; }
+
+        private void SetGroupName()
+        {
+            this.GroupName.Text = NodeGroupItem.Name;
+        }
+
+
+        public PingGroupButton()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParentPingGroupPanel.RemoveGroupe(this);
+        }
+
+       
+    }
+}
--- UIWPF/Controls/Custom/PingGroupPanel.xaml.cs
+++ UIWPF/Controls/Custom/PingGroupPanel.xaml.cs
@@ -42,16 +42,61 @@ namespace UIWPF.Controls.Custom
 
         private void AddGroup_Click(object sender, RoutedEventArgs e)
         {
+            if (DataItems == null)
+                DataItems = new ObservableCollection<NodeGroup>();
+
+            
             // Создание новой строки и кнопки внутри грида
             var newRow = new RowDefinition();
-            newRow.Height = new GridLength(50);
+            newRow.Height = new GridLength(70);
             GroupGrid.RowDefinitions.Add(newRow);
 
-            var newButton = new Button();
-            newButton.Content = "New Button";
-         
+            var newButton = new PingGroupButton();
+            var groupNodeItem = new NodeGroup();
+            groupNodeItem.Name = "Test";
+            groupNodeItem.Id = 1;
+            newButton.NodeGroupItem = groupNodeItem;
+            newButton.ParentPingGroupPanel = this;
+
+            DataItems.Add(groupNodeItem);
+
+
             Grid.SetRow(newButton, GroupGrid.RowDefinitions.Count - 1); // Устанавливаем новую кнопку в новую строку
             GroupGrid.Children.Add(newButton);
+        }
+
+        public void RemoveGroupe(PingGroupButton pingGroupButton)
+        {
+            DataItems.Remove(pingGroupButton.NodeGroupItem);
+
+            int rowIndex = Grid.GetRow(pingGroupButton);
+
+            // Если строка найдена, то удаляем ее и кнопку из Grid
+            if (rowIndex != -1)
+            {
+                GroupGrid.RowDefinitions.RemoveAt(rowIndex);
+
+                // Также удаляем кнопку из коллекции Children
+                GroupGrid.Children.Remove(pingGroupButton);
+
+                // Обновляем индексы всех остальных кнопок в Grid
+                RebuildRowIndex(rowIndex);
+            }
+            pingGroupButton.NodeGroupItem.Dispose();
+        }
+
+        private void RebuildRowIndex(int rowIndex)
+        {
+            for (int i = rowIndex; i < GroupGrid.RowDefinitions.Count; i++)
+            {
+                foreach (UIElement child in GroupGrid.Children)
+                {
+                    if (Grid.GetRow(child) == i + 1)
+                    {
+                        Grid.SetRow(child, i);
+                    }
+                }
+            }
         }
 
         public PingGroupPanel()
