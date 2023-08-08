588f372b82eb9edcd28fbe758afcc917ff32d9b0
Lexx <losangel@yandex.ru>
2023-08-08 16:01:36 +0400

    Added data validation rules.

--- VortexPings/Models/NodeGroup.cs
+++ VortexPings/Models/NodeGroup.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VortexPings.Models
+﻿namespace VortexPings.Models
 {
     public class NodeGroup:IDisposable
     {
         public int Id { get; set; }
-
+        
         public string? Name { get; set; }
 
         public int Order { get; set; }
---
+++ VortexPings/ValidationRules/UniqueNameValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VortexPings.ValidationRules
+{
+    public class UniqueNameValidationRule : IValidationRule
+    {
+        public ValidationResult Validate(object value, string? propertyName = null, Predicate<object>? isUnique = null)
+        {
+            if (isUnique == null||value==null)
+                return null;
+
+            var strValue = value as string;
+
+            
+        }
+    }
+}
